ccac6a47807afedfb4634522cb0051a0ea8ee96f
Rodrigo Fernandez <rfernandez@walmartlabs.com>
2014-04-07 17:35:26 -0700

    should serialize error instances on server

--- test/specs/shared/common/logger.js
+++ test/specs/shared/common/logger.js
@@ -37,8 +37,12 @@ describe('logger', function () {
     it('should format messages correctly', function (done) {
         sinon.stub(console, 'log');
 
+        var error = new Error('consectetur adipiscing elit');
+        error.stack = 'nullam vel tempus massa';
+
         expect(logger.error('Lorem ipsum dolor sit amet')).to.be.equal('2012-01-27T12:30:00.000Z\tERROR\tLorem ipsum dolor sit amet');
         expect(logger.error('Lorem ipsum dolor sit amet', {foo: 123, bar: 456})).to.be.equal('2012-01-27T12:30:00.000Z\tERROR\tLorem ipsum dolor sit amet {"foo":123,"bar":456}');
+        expect(logger.error('Lorem ipsum dolor sit amet', error)).to.be.equal('2012-01-27T12:30:00.000Z\tERROR\tLorem ipsum dolor sit amet {"message":"consectetur adipiscing elit","stack":"nullam vel tempus massa"}');
         expect(logger.error('Lorem %s dolor %s amet', 'ipsum', 'sit')).to.be.equal('2012-01-27T12:30:00.000Z\tERROR\tLorem ipsum dolor sit amet');
         expect(logger.error('Lorem %s dolor %s amet')).to.be.equal('2012-01-27T12:30:00.000Z\tERROR\tLorem undefined dolor undefined amet');
         expect(logger.error('Lorem ipsum dolor sit amet %d %f', 3.14159, 3.14159)).to.be.equal('2012-01-27T12:30:00.000Z\tERROR\tLorem ipsum dolor sit amet 3 3.14159');
@@ -46,7 +50,7 @@ describe('logger', function () {
         expect(logger.error(['TempoAssetUpload.UtilActions', 'upload'], 'Lorem ipsum dolor sit amet', {"foo": 123, "bar": 456})).to.be.equal('2012-01-27T12:30:00.000Z\tERROR\tLorem ipsum dolor sit amet {"foo":123,"bar":456}\tTempoAssetUpload.UtilActions.upload');
 
         setTimeout(function () {
-            expect(console.log.callCount).to.be.equal(7);
+            expect(console.log.callCount).to.be.equal(8);
             console.log.restore();
             done();
         }, 0);
