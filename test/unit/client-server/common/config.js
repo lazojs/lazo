7cd5ab902e66a0f9ca7ca3f88c5bd145f08133a1
mnajim1 <mnajim@walmartlabs.com>
2015-02-10 10:26:58 -0800

    1) Implement optional contextual parameter to LAZO.config.get(...) 2) Add documentation for Lazo's ConfigurationProvider

---
+++ test/unit/client-server/common/config.js
@@ -0,0 +1,70 @@
+define([
+    'underscore',
+    'intern!bdd',
+    'intern/chai!expect',
+    'test/unit/utils',
+    'sinon',
+    'intern/chai!',
+    'sinon-chai',
+    'lib/common/config'
+], function (_, bdd, expect, utils, sinon, chai, sinonChai, config) {
+    chai.use(sinonChai);
+
+    with (bdd) {
+
+        describe('config', function () {
+
+            config.clearPlugins(); //Clear configurations to run test in a clean state.
+
+            config.addPlugin(new function(){
+                return {
+                    data: {"key": "value"},
+
+                    get: function (key, options) {
+                        var ret;
+                        if (options && options.context) {
+                            ret = options.context[key];
+                        } else if (this.data && key in this.data) {
+                            ret = this.data[key];
+                        }
+
+                        return (options && _.isFunction(options.success)) ? options.success(ret) : ret;
+                    }
+                }
+            });
+
+
+            it('should return the value for a given key', function () {
+                expect(config.get("key")).to.be.equal('value');
+            });
+
+            it('should return the value for a given key in the provided context', function () {
+                expect(config.get("key", {context:{"key":"contextValue"}})).to.be.equal('contextValue');
+            });
+
+            it('should return the value for a given key via a success callback', function () {
+                var dfd = this.async();
+
+                config.get("key",{
+                    success: function(value){
+                        expect(value).to.be.equal('value');
+                        dfd.resolve();
+                    }
+                });
+            });
+
+            it('should return the value for a given key in the provided context via a success callback', function () {
+                var dfd = this.async();
+
+                config.get("key",{
+                    context: {"key":"contextValue"},
+                    success: function(value){
+                        expect(value).to.be.equal('contextValue');
+                        dfd.resolve();
+                    }
+                });
+            });
+
+        });
+    }
+});
